b747999cb5b048c146b7da2a9ab080c2b50a5e51
Yurchanka-Volha <ollaurcenco@gmail.com>
2026-03-06 23:21:38 +0300

    06/03/2026 note. Go code

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 
 03/03/2026
 1. EPAM English test (A2)
+2. Git 
+
+06/03/2026
+1. English test EFSET: reading - A1, audio - A1, writing - B1, speacking - B1.
 2. Git
+3. Go (Introduction, variables, fmt package)
--- test_go_files/test.go
+++ test_go_files/test.go
@@ -1,1 +1,17 @@
-1
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string
+	fmt.Println("Hi! I'm your new device! Pls write username:")
+	fmt.Scan(&name)
+	fmt.Println("Hello,", name, ",nice to meet you!")
+
+	var age int
+	fmt.Println("Pls write your age:")
+	fmt.Scan(&age)
+	fmt.Printf("OK! Username - %s, age - %d\n", name, age)
+}
